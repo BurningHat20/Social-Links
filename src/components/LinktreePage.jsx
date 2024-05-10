b6fedb69fa2c5488597ee1b16f4d98b5fa1ed108
BurningHat12 <yashgohel395@gmail.com>
2024-05-10 17:28:36 +0530

    Added Categories buttons

--- src/components/LinktreePage.jsx
+++ src/components/LinktreePage.jsx
@@ -1,79 +1,49 @@
 import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
-import {
-  FaGlobe,
-  FaInstagram,
-  FaLinkedinIn,
-  FaExternalLinkAlt,
-  FaSnapchatGhost,
-  FaGithub,
-  FaSun,
-  FaMoon,
-} from "react-icons/fa";
-import { FaXTwitter, FaThreads } from "react-icons/fa6";
-import { IoIosMail, IoIosSunny } from "react-icons/io";
-import { GrDocumentPdf } from "react-icons/gr";
+import { motion, AnimatePresence } from "framer-motion";
+import { FaExternalLinkAlt, FaSun, FaMoon } from "react-icons/fa";
+import { IoIosSunny } from "react-icons/io";
+import classnames from "classnames";
+import { links } from "../constants";
 import AuthorImage from "../assets/Yash-Gohel.jpg";
 
-const links = [
-  {
-    id: 1,
-    title: "Portfolio Website",
-    url: "https://burninghat.tech",
-    icon: FaGlobe,
-  },
-  {
-    id: 2,
-    title: "LinkedIn",
-    url: "https://www.linkedin.com/in/yashgohel777/",
-    icon: FaLinkedinIn,
-  },
-  {
-    id: 3,
-    title: "Twitter",
-    url: "https://twitter.com/yashgohel_",
-    icon: FaXTwitter,
-  },
-  {
-    id: 4,
-    title: "Github",
-    url: "https://github.com/BurningHat20/",
-    icon: FaGithub,
-  },
-  {
-    id: 5,
-    title: "Mail",
-    url: "mailto:burninghat20@gmail.com",
-    icon: IoIosMail,
-  },
-  {
-    id: 6,
-    title: "Resume",
-    url: "https://drive.google.com/file/d/1D2cpUku3Y0NrFDQUYskLhJJA5VSLL-qQ/view?usp=sharing",
-    icon: GrDocumentPdf,
-  },
-  {
-    id: 7,
-    title: "Instagram",
-    url: "https://www.instagram.com/yashdgaf._",
-    icon: FaInstagram,
-  },
-  {
-    id: 8,
-    title: "Snapchat",
-    url: "https://www.snapchat.com/add/yashh_312?share_id=brjf_cQLyM4&locale=en-IN",
-    icon: FaSnapchatGhost,
-  },
-  {
-    id: 9,
-    title: "Threads",
-    url: "https://www.threads.net/@yashdgaf._",
-    icon: FaThreads,
-  },
-];
+const Link = ({ link, isLightTheme }) => (
+  <motion.a
+    href={link.url}
+    target="_blank"
+    rel="noopener noreferrer"
+    className={classnames(
+      "block rounded-lg p-4 mb-4 transition-all duration-300 shadow-lg flex items-center justify-between mx-4 font-montserrat",
+      {
+        "bg-gray-200 hover:bg-gray-300 text-gray-900": isLightTheme,
+        "bg-gray-800 hover:bg-gray-700 text-white": !isLightTheme,
+      }
+    )}
+    initial={{ opacity: 0, y: -20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.5 }}
+  >
+    <motion.span
+      className="flex items-center"
+      initial={{ opacity: 0, x: -20 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.3, delay: 0.5 }}
+    >
+      <link.icon className="mr-4 text-xl" />
+      <span className="text-lg font-medium">{link.title}</span>
+    </motion.span>
+    <motion.span
+      initial={{ opacity: 0, x: 20 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.3, delay: 0.5 }}
+    >
+      <FaExternalLinkAlt className="text-lg" />
+    </motion.span>
+  </motion.a>
+);
 
 const LinktreePage = () => {
   const [isLightTheme, setIsLightTheme] = useState(true);
+  const [selectedCategory, setSelectedCategory] = useState("All");
 
   useEffect(() => {
     const systemPrefersDark = window.matchMedia(
@@ -86,118 +56,98 @@ const LinktreePage = () => {
     setIsLightTheme((prevTheme) => !prevTheme);
   };
 
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const filteredLinks =
+    selectedCategory === "All"
+      ? links
+      : links.filter((link) => link.category === selectedCategory);
+
   return (
     <div
-      className={
-        isLightTheme
-          ? "bg-gray-100 min-h-screen text-gray-900"
-          : "bg-gray-900 min-h-screen text-white"
-      }
+      className={classnames("min-h-screen flex flex-col justify-between", {
+        "text-gray-900 bg-gray-100": isLightTheme,
+        "text-white bg-gray-900": !isLightTheme,
+      })}
     >
-      <div className="py-8 text-center relative">
-        <motion.img
-          src={AuthorImage}
-          alt="Author"
-          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-gray-700 shadow-lg"
-          style={{ objectFit: "cover" }}
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-        />
-        <motion.h1
-          className={
-            isLightTheme
-              ? "text-4xl font-bold mb-2 font-ubuntu text-gray-900"
-              : "text-4xl font-bold mb-2 font-ubuntu"
-          }
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 0.2 }}
-        >
-          My Linktree
-        </motion.h1>
-        <motion.p
-          className={
-            isLightTheme
-              ? "text-lg mb-5 font-palanquin text-gray-700"
-              : "text-lg mb-5 font-palanquin"
-          }
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 0.4 }}
-        >
-          Connect with me on various platforms
-        </motion.p>
-        <button
-          className="absolute top-0 right-0 m-4 p-2 rounded-full bg-gray-800 text-white"
-          onClick={toggleTheme}
-        >
-          {isLightTheme ? <FaMoon /> : <IoIosSunny />}
-        </button>
-      </div>
-      <div className="max-w-4xl mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
-          {links.map((link) => (
-            <motion.a
-              key={link.id}
-              href={link.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={
-                isLightTheme
-                  ? "block bg-gray-200 hover:bg-gray-300 rounded-lg p-4 mb-4 transition-all duration-300 shadow-lg flex items-center justify-between mx-4 font-montserrat"
-                  : "block bg-gray-800 hover:bg-gray-700 rounded-lg p-4 mb-4 transition-all duration-300 shadow-lg flex items-center justify-between mx-4 font-montserrat"
-              }
-              initial={{ opacity: 0, y: -20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5 }}
-            >
-              <motion.span
-                className="flex items-center"
-                initial={{ opacity: 0, x: -20 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.3, delay: 0.5 }}
-              >
-                <link.icon className="mr-4 text-xl" />
-                <span
-                  className={
-                    isLightTheme
-                      ? "text-lg font-medium text-gray-900"
-                      : "text-lg font-medium text-white"
-                  }
-                >
-                  {link.title}
-                </span>
-              </motion.span>
-              <motion.span
-                initial={{ opacity: 0, x: 20 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.3, delay: 0.5 }}
-              >
-                <FaExternalLinkAlt className="text-lg" />
-              </motion.span>
-            </motion.a>
-          ))}
-        </div>
-        <motion.p
-          className={
-            isLightTheme
-              ? "text-sm mt-8 pb-4 text-gray-700 text-center font-palanquin"
-              : "text-sm mt-8 pb-4 text-gray-400 text-center font-palanquin"
-          }
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 0.7 }}
-        >
-          <a
-            href="https://burninghat.tech"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div>
+        <div className="py-8 text-center relative">
+          <motion.img
+            src={AuthorImage}
+            alt="Author"
+            className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-gray-700 shadow-lg"
+            style={{ objectFit: "cover" }}
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+          />
+          <motion.h1
+            className="text-4xl font-bold mb-2 font-ubuntu"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
           >
-            © 2024 BurningHat. All rights reserved.
-          </a>
-        </motion.p>
+            My Linktree
+          </motion.h1>
+          <motion.p
+            className="text-lg mb-5 font-palanquin"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+          >
+            Connect with me on various platforms
+          </motion.p>
+          <div className="mb-3 ml-1 flex justify-center">
+            {["All", "Social Media", "Portfolio", "Contact"].map((category) => (
+              <button
+                key={category}
+                className={classnames("mr-2 px-4 py-2 rounded-md", {
+                  "bg-gray-800 text-white": selectedCategory === category,
+                  "bg-gray-200 text-gray-900": selectedCategory !== category,
+                })}
+                onClick={() => handleCategoryClick(category)}
+              >
+                {category}
+              </button>
+            ))}
+          </div>
+          <button
+            className="absolute top-0 right-0 m-4 p-2 rounded-full bg-gray-800 text-white"
+            onClick={toggleTheme}
+          >
+            {isLightTheme ? <FaMoon /> : <IoIosSunny />}
+          </button>
+        </div>
+        <div className="max-w-4xl mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
+            <AnimatePresence>
+              {filteredLinks.map((link) => (
+                <Link
+                  key={selectedCategory + "-" + link.id}
+                  link={link}
+                  isLightTheme={isLightTheme}
+                />
+              ))}
+            </AnimatePresence>
+          </div>
+        </div>
       </div>
+      <motion.p
+        className="text-sm mt-8 pb-4 text-center font-palanquin"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.7 }}
+      >
+        <a
+          href="https://burninghat.tech"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          © 2024 BurningHat. All rights reserved.
+        </a>
+      </motion.p>
     </div>
   );
 };
---
+++ src/constants/index.js
@@ -0,0 +1,79 @@
+import {
+  FaGlobe,
+  FaInstagram,
+  FaLinkedinIn,
+  FaExternalLinkAlt,
+  FaSnapchatGhost,
+  FaGithub,
+  FaSun,
+  FaMoon,
+} from "react-icons/fa";
+import { FaXTwitter, FaThreads } from "react-icons/fa6";
+import { IoIosMail, IoIosSunny } from "react-icons/io";
+import { GrDocumentPdf } from "react-icons/gr";
+
+export const links = [
+  {
+    id: 1,
+    title: "Portfolio Website",
+    url: "https://burninghat.tech",
+    icon: FaGlobe,
+    category: "Portfolio",
+  },
+  {
+    id: 2,
+    title: "LinkedIn",
+    url: "https://www.linkedin.com/in/yashgohel777/",
+    icon: FaLinkedinIn,
+    category: "Social Media",
+  },
+  {
+    id: 3,
+    title: "Twitter",
+    url: "https://twitter.com/yashgohel_",
+    icon: FaXTwitter,
+    category: "Social Media",
+  },
+  {
+    id: 4,
+    title: "Github",
+    url: "https://github.com/BurningHat20/",
+    icon: FaGithub,
+    category: "Portfolio",
+  },
+  {
+    id: 5,
+    title: "Mail",
+    url: "mailto:burninghat20@gmail.com",
+    icon: IoIosMail,
+    category: "Contact",
+  },
+  {
+    id: 6,
+    title: "Resume",
+    url: "https://drive.google.com/file/d/1D2cpUku3Y0NrFDQUYskLhJJA5VSLL-qQ/view?usp=sharing",
+    icon: GrDocumentPdf,
+    category: "Portfolio",
+  },
+  {
+    id: 7,
+    title: "Instagram",
+    url: "https://www.instagram.com/yashdgaf._",
+    icon: FaInstagram,
+    category: "Social Media",
+  },
+  {
+    id: 8,
+    title: "Snapchat",
+    url: "https://www.snapchat.com/add/yashh_312?share_id=brjf_cQLyM4&locale=en-IN",
+    icon: FaSnapchatGhost,
+    category: "Social Media",
+  },
+  {
+    id: 9,
+    title: "Threads",
+    url: "https://www.threads.net/@yashdgaf._",
+    icon: FaThreads,
+    category: "Social Media",
+  },
+];
